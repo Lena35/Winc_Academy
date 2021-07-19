5cb04e400b2e5cf8b4251f20326bbb0506ca3ba2
Lena35 <lenalee357@gmail.com>
2021-07-19 14:54:59 +0200

    opdr 9

---
+++ opdracht_9/script.js
@@ -0,0 +1,12 @@
+console.log('Hello Winc Academy');
+let name = 'Andrea';
+console.log(name);
+var a = 4;
+var b = 4;
+console.log(a+b);
+console.log(a*b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+var age = '26';
+console.log(typeof age);
