14ff2ffeb7a6a0e4966e183543261ca62a0ba719
Lena35 <lenalee357@gmail.com>
2021-07-19 19:58:00 +0200

    opdr 11

---
+++ opdracht_11/script.js
@@ -0,0 +1,32 @@
+const age = 26;
+if (age >= 18)
+{
+    console.log('Je mag naar binnen!');
+}
+
+else
+{
+    console.log('Je bent te jong, je mag niet  naar binnen!');
+}
+
+const isFemale = true
+if (isFemale == true)
+{
+    console.log('Je mag naar binnen!');
+}
+
+else
+{
+    console.log('Je bent geen vrouw, je mag niet naar binnen!');
+}
+
+const driverStatus = 'bob';
+if (driverStatus == 'bob')
+{
+    console.log('Je bent bob, je mag gaan rijden!');
+}
+
+else
+{
+    console.log('Je bent niet de bob, je mag niet rijden!');
+}
