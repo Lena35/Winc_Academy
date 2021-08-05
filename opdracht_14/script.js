cc7ad94e59825e80bed164c7fadcc7ffb31fb61e
Lena35 <lenalee357@gmail.com>
2021-08-05 14:49:53 +0200

    opdr 14

---
+++ opdracht_14/script.js
@@ -0,0 +1,17 @@
+function makePizza()
+{
+    console.log("Shape the dough");
+    console.log("Add toppings");
+    console.log("Put it in the oven");
+}
+
+function makeSushi()
+{
+    console.log("Roll sushi");
+}
+
+makePizza();
+makePizza();
+makeSushi();
+makeSushi();
+makeSushi();
