5bea0d99ab41746cb658e4853c50c25be87d4359
Lena35 <lenalee357@gmail.com>
2021-08-18 15:39:33 +0200

    opdr 17

---
+++ opdracht_17/script.js
@@ -0,0 +1,32 @@
+function square(num1, num2)
+{
+    num3 = num1 * num1;
+    num4 = num2 * num2;
+    num5 = num3 + num4;
+    num6 = num5 * num5;
+    return num6;
+}
+
+console.log(square(3, 8));
+
+const number = function (num1, num2)
+{
+    num3 = num1 * num1;
+    num4 = num2 * num2;
+    num5 = num3 + num4;
+    num6 = num5 * num5;
+    return num6;
+};
+
+console.log(square(3, 8));
+
+const add = (num1, num2) =>
+{
+    num3 = num1 * num1;
+    num4 = num2 * num2;
+    num5 = num3 + num4;
+    num6 = num5 * num5;
+    return num6;
+}
+
+console.log(add(3, 8));
